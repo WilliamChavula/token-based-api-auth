5f0d11435dcd16c9185cd819bc98f5283848b220
William Chavula <rumbani@gmail.com>
2025-01-27 10:00:29 -0600

    feat: ensure passwords are hashed before inserting into db

--- src/lib/auth/services/index.ts
+++ src/lib/auth/services/index.ts
@@ -3,6 +3,7 @@ import fp from "fastify-plugin";
 
 import type { TInsertUserSchema, TSelectUserSchema } from "../schemas";
 import { usersTable } from "../models";
+import { bcryptHash } from "../utilities";
 
 export interface TAuthService {
   create: (data: TInsertUserSchema) => Promise<TSelectUserSchema>;
@@ -11,9 +12,13 @@ export interface TAuthService {
 export default fp(async function (fastify: FastifyInstance) {
   fastify.decorate<TAuthService>("authService", {
     async create(data: TInsertUserSchema): Promise<TSelectUserSchema> {
+      const { password } = data;
+
+      const hashedPassword = await bcryptHash.hash(password);
+
       const [user] = await fastify.db
         .insert(usersTable)
-        .values(data)
+        .values({ ...data, password: hashedPassword })
         .returning();
 
       return user;
