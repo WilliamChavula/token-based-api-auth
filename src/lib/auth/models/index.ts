3930b6eeddbb816e2d6e073ece450be9e9f06eab
William Chavula <rumbani@gmail.com>
2025-01-27 09:31:36 -0600

    feat: define a table that will store tokens issued to users after sign-in

--- src/lib/auth/models/index.ts
+++ src/lib/auth/models/index.ts
@@ -1,4 +1,10 @@
-import { pgTable, serial, varchar, timestamp } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  varchar,
+  timestamp,
+  index,
+} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   userId: serial("user_id").primaryKey().notNull(),
@@ -10,3 +16,18 @@ export const usersTable = pgTable("users", {
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
 });
+
+export const tokenTable = pgTable(
+  "tokens",
+  {
+    tokenId: varchar("token_id").primaryKey().notNull(),
+    userId: varchar("user_id", { length: 30 }).notNull(),
+    expiry: timestamp().notNull(),
+  },
+  (table) => {
+    return [
+      index("username_idx").on(table.userId),
+      index("expiry_idx").on(table.expiry),
+    ];
+  },
+);
