4ab1133d4f8d277a75628b9a8cfcf025d49f88fd
William Chavula <rumbani@gmail.com>
2025-01-27 20:52:21 -0600

    feat: add hash_key env variable for token hashing with hmac

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 
 const environmentVars = z.object({
   DB_URL: z.string({ required_error: "Database URL cannot be empty" }),
+  HASH_KEY: z.string({ required_error: "Hash key cannot be empty" }),
 });
 
 export const config = environmentVars.parse(process.env);
