88d3972df778b0d31423d922d1dadfd03ab68ae1
William Chavula <rumbani@gmail.com>
2025-01-27 20:55:25 -0600

    feat: add login schema for login data validation

--- src/lib/auth/schemas/index.ts
+++ src/lib/auth/schemas/index.ts
@@ -4,15 +4,25 @@ import { buildJsonSchemas } from "fastify-zod";
 
 import { usersTable } from "../models";
 
-const insertUserSchema = createInsertSchema(usersTable);
+const insertUserSchema = createInsertSchema(usersTable, {
+  password: (schema) => schema.min(8).max(20),
+  email: (schema) => schema.email(),
+});
 const selectUserSchema = createSelectSchema(usersTable).omit({
   password: true,
+});
+
+const loginUserSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
 });
 
 export const { schemas, $ref } = buildJsonSchemas({
   insertUserSchema,
   selectUserSchema,
+  loginUserSchema,
 });
 
 export type TInsertUserSchema = z.infer<typeof insertUserSchema>;
 export type TSelectUserSchema = z.infer<typeof selectUserSchema>;
+export type TLoginUserSchema = z.infer<typeof loginUserSchema>;
