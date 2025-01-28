156f15ccf26686eae7ab1a6f9999ec88b58a7f68
William Chavula <rumbani@gmail.com>
2025-01-27 20:53:03 -0600

    chore: create barrel file for auth utilities

--- src/lib/auth/utilities/index.ts
+++ src/lib/auth/utilities/index.ts
@@ -1,12 +1,2 @@
-import bcrypt from "bcrypt";
-import { THash } from "./types";
-
-export const bcryptHash: THash = {
-  async hash(pwd: string, rounds: number = 10): Promise<string> {
-    return bcrypt.hash(pwd, rounds);
-  },
-
-  async compare(pwd: string, hash: string): Promise<boolean> {
-    return bcrypt.compare(pwd, hash);
-  },
-};
+export { bcryptHash } from "./bcrypt.utilities";
+export { tokenStore } from "./encrypt.utilities";
