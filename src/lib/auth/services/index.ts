d4ff1adf504ba18637f60ed94ed534a4c5df86e5
William Chavula <rumbani@gmail.com>
2025-01-27 20:54:53 -0600

    feat: add auth service method implementations for creating users and managing tokens

--- src/lib/auth/services/index.ts
+++ src/lib/auth/services/index.ts
@@ -1,17 +1,26 @@
 import type { FastifyInstance } from "fastify";
 import fp from "fastify-plugin";
+import { eq, InferSelectModel } from "drizzle-orm";
 
 import type { TInsertUserSchema, TSelectUserSchema } from "../schemas";
-import { usersTable } from "../models";
-import { bcryptHash } from "../utilities";
+import { tokenTable, usersTable } from "../models";
+import { bcryptHash, tokenStore } from "../utilities";
+import { TAuthToken } from "../utilities/types";
+
+type User = InferSelectModel<typeof usersTable>;
 
 export interface TAuthService {
-  create: (data: TInsertUserSchema) => Promise<TSelectUserSchema>;
+  createUser: (data: TInsertUserSchema) => Promise<TSelectUserSchema>;
+  getUserByEmail: (email: string) => Promise<User | null>;
+  createToken: (email: string) => Promise<string>;
+  deleteToken: (tokenId: string) => Promise<void>;
+  readToken: (tokenId: string) => Promise<TAuthToken | null>;
+  verifyToken: (token: string) => Promise<TAuthToken | null>;
 }
 
 export default fp(async function (fastify: FastifyInstance) {
   fastify.decorate<TAuthService>("authService", {
-    async create(data: TInsertUserSchema): Promise<TSelectUserSchema> {
+    async createUser(data: TInsertUserSchema): Promise<TSelectUserSchema> {
       const { password } = data;
 
       const hashedPassword = await bcryptHash.hash(password);
@@ -22,6 +31,83 @@ export default fp(async function (fastify: FastifyInstance) {
         .returning();
 
       return user;
+    },
+
+    async getUserByEmail(email: string): Promise<User | null> {
+      const [user] = await fastify.db
+        .select()
+        .from(usersTable)
+        .where(eq(usersTable.email, email));
+
+      return user;
+    },
+
+    async createToken(email: string): Promise<string> {
+      const [user] = await fastify.db
+        .select()
+        .from(usersTable)
+        .where(eq(usersTable.email, email));
+
+      if (!user) throw new Error("User not found");
+
+      const token = tokenStore.generateTokenId();
+      const expiry = new Date(Date.now() + 1000 * 60 * 60 * 24);
+
+      await fastify.db
+        .insert(tokenTable)
+        .values({ userId: user.userId, tokenId: token, expiry });
+
+      return token;
+    },
+
+    async readToken(tokenId: string): Promise<TAuthToken | null> {
+      const [token] = await fastify.db
+        .select()
+        .from(tokenTable)
+        .where(eq(tokenTable.tokenId, tokenId));
+
+      if (!token) return null;
+
+      return {
+        userId: token.userId,
+        tokenId: token.tokenId,
+        expiry: token.expiry,
+      } as TAuthToken;
+    },
+
+    async deleteToken(tokenId: string): Promise<void> {
+      await fastify.db
+        .delete(tokenTable)
+        .where(eq(tokenTable.tokenId, tokenId));
+    },
+
+    async verifyToken(tokenRaw: string): Promise<TAuthToken | null> {
+      const tokenIndex = tokenRaw.lastIndexOf(".");
+
+      if (tokenIndex === -1) return null;
+
+      const token = tokenRaw.substring(0, tokenIndex);
+
+      const tokenData = await fastify.authService.readToken(token);
+
+      if (!tokenData) return null;
+
+      const tokenHash = tokenRaw.substring(tokenIndex + 1);
+
+      const computedHash = tokenStore.computeHash(tokenData.tokenId);
+      console.log({ computedHash, tokenHash });
+
+      if (!tokenStore.compareHash(tokenHash, computedHash)) return null;
+
+      const { expiry } = tokenData;
+
+      if (expiry < new Date()) {
+        await fastify.authService.deleteToken(token);
+
+        return null;
+      }
+
+      return tokenData;
     },
   });
 });
