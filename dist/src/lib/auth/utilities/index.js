c27d2a240d3fa90aaf4bd7edac43dfcb883c68dc
William Chavula <rumbani@gmail.com>
2025-06-20 14:29:39 -0500

    chore: made some changes

--- dist/src/lib/auth/utilities/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tokenStore = exports.bcryptHash = void 0;
-var bcrypt_utilities_1 = require("./bcrypt.utilities");
-Object.defineProperty(exports, "bcryptHash", { enumerable: true, get: function () { return bcrypt_utilities_1.bcryptHash; } });
-var encrypt_utilities_1 = require("./encrypt.utilities");
-Object.defineProperty(exports, "tokenStore", { enumerable: true, get: function () { return encrypt_utilities_1.tokenStore; } });
--- dist/src/lib/db/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-var node_postgres_1 = require("drizzle-orm/node-postgres");
-var config_1 = require("../config");
-exports.db = (0, node_postgres_1.drizzle)(config_1.config.DB_URL);
